94928d612689388c9abb17e06ebe31e81cc6dc24
Toran Sahu <toran.sahu@yahoo.com>
2018-11-10 09:01:20 +0530

    Initial Commit

---
+++ src/aijaa/dev_settings.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-10 08:59
+
+"""
+dev_settings.py
+"""
+
+
+from .base_settings import *
+
+
+__author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the MIT license'
